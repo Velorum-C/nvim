bacda57f66dbecdcdc7c9c5339c33f70cde45de9
Velorum-C <m.altaykan@outlook.com>
2023-10-20 21:15:54 -0400

    fixed indent_blankline

---
+++ lua/met/indent_blankline.lua
@@ -0,0 +1,11 @@
+local status_ok, indent_blankline = pcall(require, "ibl")
+if not status_ok then
+	return
+end
+
+-- -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+vim.wo.colorcolumn = "99999"
+
+indent_blankline.setup({
+	indent = { char = "|" }
+})
