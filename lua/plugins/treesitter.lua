0d8848ffe9186cadcb0f3a355abd7144655111c5
Velorum-C <m.altaykan@outlook.com>
2024-01-15 16:15:37 +0000

    Updated treesitter part 1

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,53 +1,48 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		"p00f/nvim-ts-rainbow",
 		"windwp/nvim-ts-autotag",
 		"windwp/nvim-autopairs",
 	},
-	opt = {
-		ensure_installed = {
-			"c",
-			"lua",
-			"vim",
-			"help",
-			"bash",
-			"javascript",
-			"json",
-			"python",
-			"typescript",
-			"tsx",
-			"css",
-			"rust",
-			"java",
-			"yaml",
-			"markdown",
-			"markdown_inline",
-		},               -- one of "all" or a list of languages
-		ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-		auto_install = true, -- Auto install a parser when entering a file
-		highlight = {
-			enable = true, -- false will disable the whole extension
-			-- disable = { "" }, -- list of language that will be disabled
-		},
-		autopairs = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-			disable = { "python" },
-		},
-		autotag = {
-			enable = true,
-		},
-		rainbow = {
-			enable = true,
-			-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-			extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-			max_file_lines = nil, -- Do not enable for files with more than n lines, int
-			-- colors = {}, -- table of hex strings
-			-- termcolors = {} -- table of colour name strings
-		},
-	}
-}
+	config = function()
+		local ts = require("nvim-treesitter.configs")
 
+		ts.setup({
+			ensure_installed = {
+				"c",
+				"cpp",
+				"lua",
+				"vim",
+				"bash",
+				"javascript",
+				"json",
+				"python",
+				"typescript",
+				"tsx",
+				"css",
+				"rust",
+				"java",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+			}, -- one of "all" or a list of languages
+			ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+			auto_install = true, -- Auto install a parser when entering a file
+			highlight = { enable = true }, -- false will disable the whole extension
+			indent = { enable = true, disable = { "python" } },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			autopairs = { enable = true },
+			autotag = { enable = true },
+		})
+	end,
+}
